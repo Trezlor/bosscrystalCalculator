4c69d637fbad0bb5232293b5af74e70c99c29199
Trezlor <77504889+Trezlor@users.noreply.github.com>
2023-12-24 12:11:52 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BosscrystalCalulator from './pages/bosscrystalCalculator/BosscrystalCalulator';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<BosscrystalCalulator />
+		</div>
+	);
 }
 
 export default App;
