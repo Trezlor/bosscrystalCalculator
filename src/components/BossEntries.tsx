646c864f2990982a8aa08d2d452729861762350f
Trezlor <sondre-h@hotmail.com>
2024-01-03 23:11:20 +0100

    fix: mui list & semantics used properly

--- src/components/BossEntries.tsx
+++ src/components/BossEntries.tsx
@@ -1,43 +1,53 @@
-import { Button, Divider, Grid, ListItem, Stack } from '@mui/material';
+import { Collapse, List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import { BossProps } from '../utils/props';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import { useState } from 'react';
 
-const BossEntries = (props: BossProps) => {
+const BossEntries = (boss: BossProps) => {
+	const [open, setOpen] = useState<boolean>(false);
+	const handleClick = () => {
+		setOpen(!open);
+	};
+
 	return (
 		<>
-			<ListItem
-				key={props.id}
-				disablePadding
-				sx={props.lowestDifficulty && props.name !== 'Zakum' ? { mt: 4 } : { mt: 0 }}
+			<ListItemButton
+				onClick={handleClick}
+				sx={{
+					borderWidth: 4,
+					borderStyle: 'solid',
+					...(boss.lowestDifficulty && boss.name !== 'Zakum' ? { mt: 2 } : { mt: 0 }),
+				}}
+				className={`difficulty${boss.difficulty}`}
 			>
-				<Grid
-					direction='row'
-					spacing={2}
-					width='100%'
-					justifyContent='center'
-					bgcolor={'white'}
-					px={2}
-					py={1}
+				<ListItemIcon>
+					<img
+						src={require(`../assets/inGameImages/${boss.name.replace(/\s+/g, '')}.png`)}
+						alt={boss.name}
+						loading='lazy'
+						style={{ height: 80, objectFit: 'contain' }}
+					/>
+				</ListItemIcon>
+
+				<ListItemText primary={boss.difficulty + ' ' + boss.name} />
+
+				{open ? <ExpandLess /> : <ExpandMore />}
+			</ListItemButton>
+			<Collapse
+				in={open}
+				timeout='auto'
+				unmountOnExit
+			>
+				<List
+					component='div'
+					disablePadding
+					sx={{ pl: 8 }}
 				>
-					<img
-						src={require(`../assets/smallSizeImages/${props.name.replace(/\s+/g, '')}.png`)}
-						alt={props.name}
-						loading='lazy'
-						style={{ width: 50, height: 50 }}
-					/>
-					<img
-						src={require(`../assets/bossDifficulty/${props.difficulty}.png`)}
-						alt={props.name}
-						loading='lazy'
-						style={{ width: 100 }}
-					/>
-					<Button
-						sx={{ height: 'fit-content' }}
-						variant='contained'
-					>
-						More
-					</Button>
-				</Grid>
-			</ListItem>
+					<ListItemIcon>{/* <StarBorder /> */}</ListItemIcon>
+					<ListItemText primary='Starred' />
+				</List>
+			</Collapse>
 		</>
 	);
 };
--- src/pages/bosscrystalCalculator/BosscrystalCalculator.tsx
+++ src/pages/bosscrystalCalculator/BosscrystalCalculator.tsx
@@ -1,27 +1,27 @@
-import { List } from '@mui/material';
+import { List, Stack } from '@mui/material';
 import { BossEntries } from '../../components/BossEntries';
 import bossList from '../../data/bossList.json';
-import { PageContainer } from './BosscrystalCalculator.styles';
 
 const BosscrystalCalculator = () => {
 	return (
-		<PageContainer>
-			<section>
-				<List disablePadding>
-					{bossList.map((boss) => (
-						<BossEntries
-							name={boss.name}
-							value={boss.value}
-							difficulty={boss.difficulty}
-							lowestDifficulty={boss.lowestDifficulty}
-							initialValue={boss.initialValue}
-							id={boss.id}
-							key={boss.id}
-						/>
-					))}
-				</List>
-			</section>
-		</PageContainer>
+		<Stack alignItems={'center'}>
+			<List
+				disablePadding
+				sx={{ width: '100%', maxWidth: 1220 }}
+			>
+				{bossList.map((boss) => (
+					<BossEntries
+						name={boss.name}
+						value={boss.value}
+						difficulty={boss.difficulty}
+						lowestDifficulty={boss.lowestDifficulty}
+						initialValue={boss.initialValue}
+						id={boss.id}
+						key={boss.id}
+					/>
+				))}
+			</List>
+		</Stack>
 	);
 };
 
